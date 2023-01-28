dad76c93a1e9786dd698aa78e9f32764814d3a80
Jebin Jacob John <jebinjacobjohn2001@gmail.com>
2023-01-28 21:41:15 +0530

    commit

---
+++ bin/sample2.dart
@@ -0,0 +1,18 @@
+// to print my phone info
+
+void main()
+{
+  String pname = 'Realme x3 Superzoom';
+  String ram ='8GB' ;
+  String storage = '128GB';
+  String gsm = ' Non 5G';
+  String sim = 'Dual Sim';
+
+  print(" Phone Details");
+  print(" Phone Name = $pname");
+  print(" RAM = $ram");
+  print(" Storage = $storage");
+  print(" 5G or not = $gsm");
+  print(" sim type = $sim");
+
+}
---
+++ bin/variables.dart
@@ -0,0 +1,16 @@
+// to print my info
+void main()
+{
+  String name =" Jebin";
+      int age = 22;
+      double mark = 8.4;
+      String email = 'abc@gmail.com';
+  int phone = 1234567890;
+  print(' My Details');
+      print("name = $name");
+  print('age = $age');
+  print('mark = $mark');
+  print('email = $email');
+  print('phone = $phone');
+}
+
