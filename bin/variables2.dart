97cbe8b25630de00346cd2f9909b6e352e8180f1
Jebin Jacob John <jebinjacobjohn2001@gmail.com>
2023-02-10 22:45:13 +0530

    commit

---
+++ bin/variables2.dart
@@ -0,0 +1,16 @@
+void main()
+{
+  String name ="Jebin";
+  int age = 23;
+  int phone = 1234567890;
+  double mark = 8.3;
+  String email= "jebinjj@gmail.com";
+
+  print(" My Resume");
+  print("My name is $name");
+  print("I am $age years old");
+  print(" My phone number is $phone");
+  print("I have $mark cgpa in graduation");
+  print(" My e-mail id is $email");
+
+}
