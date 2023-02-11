68e28f9d7fb90ef880984195a5d289c77fc3dc9f
Jebin Jacob John <jebinjacobjohn2001@gmail.com>
2023-02-11 18:05:19 +0530

    commit

---
+++ bin/var,dynamic.dart
@@ -0,0 +1,10 @@
+void main()
+{
+  var name = "Jebin";
+  var age = 23;
+  var mark= 7.8;
+   print('My name is $name');
+   print('I am $age years old');
+   print('My cgpa in graduation is$mark');
+
+}
